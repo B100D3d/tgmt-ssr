803de55b51cc7b8febfc96578dbeb3a93da78a1e
devourer <alexblooded@gmail.com>
2020-04-08 18:35:21 +0300

    Fix favicon href

--- src/server.js
+++ src/server.js
@@ -81,7 +81,7 @@ const getHtml = async (markup, extractor, helmet, initialData, ua) => {
 		${helmet.title.toString()}
 		${helmet.meta.toString()}
 		${styles}
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 		<link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap&subset=cyrillic" rel="stylesheet">
 	</head>
 	  <body>
