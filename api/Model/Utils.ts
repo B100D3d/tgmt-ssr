047c0b5ff569cfe6f8611ec4ea8565418dafa474
devourer <alexblooded@gmail.com>
2020-08-24 18:25:02 +0300

    fix: range function

--- api/Model/Utils.ts
+++ api/Model/Utils.ts
@@ -7,7 +7,7 @@ export const range = (from: number, to?: number, step = 1): Array<number> => {
     if (!to) {
         ;[from, to] = [0, from]
     }
-    const size = to - from
+    const size = Math.floor((to - from) / step)
     return [...Array(size).keys()].map((k) => from + k * step)
 }
 
